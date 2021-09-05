04ed52300785453223937ee818476716bdc08da1
Tyler Nijmeh <tylernij@gmail.com>
2021-09-04 18:05:39 -0700

    Run script on device locally
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- inject.sh
+++ inject.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
-TMPFILE="$(mktemp)"
-ISSH_REMOTE_PATH="/sdcard/issh"
+ISSH_DIR="/sdcard/.issh"
+ISSH_PATH="$ISSH_DIR/issh"
 ISSH_RAW_URL="https://raw.githubusercontent.com/tytydraco/issh/main/issh"
 
-# Wait for a device to be connected
-adb wait-for-device
+# Make the issh files directory if it does not yet exist
+mkdir -p "$ISSH_DIR"
 
-# Pull latest issh script
-curl -sLo "$TMPFILE" "$ISSH_RAW_URL"
-
-# Push issh script to device temporary directory
-adb push "$TMPFILE" "$ISSH_REMOTE_PATH"
+# Pull latest issh script to the device
+curl -sLo "$ISSH_PATH" "$ISSH_RAW_URL"
 
 # Kill any running issh servers
-adb shell "pkill toybox"
+pkill toybox
 
 # Fork issh daemon for localhost connections only
-# We also fork the shell command here as it tends to get stuck
-adb shell "sh $ISSH_REMOTE_PATH -dl" &
-
-# Press the power button to indicate completion
-adb shell input keyevent 26
-
+sh "$ISSH_PATH" -dl &
