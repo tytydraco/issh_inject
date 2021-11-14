0b97b770ad0939d6ef35b2ea6664eda12dc63963
Tyler Nijmeh <tylernij@gmail.com>
2021-11-14 02:08:55 -0800

    Make it pretty
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- inject.sh
+++ inject.sh
@@ -1,16 +1,44 @@
 #!/usr/bin/env bash
+
 ISSH_DIR="/sdcard/.issh"
 ISSH_PATH="$ISSH_DIR/issh"
 ISSH_RAW_URL="https://raw.githubusercontent.com/tytydraco/issh/main/issh"
 
+dbg() {
+    echo -e "\e[36m * $*\e[0m"
+}
+
 # Make the issh files directory if it does not yet exist
-mkdir -p "$ISSH_DIR"
+prepare_env() {
+    dbg "Preparing the device environment..."
+    adb shell "mkdir -p $ISSH_DIR"
+}
 
-# Pull latest issh script to the device
-curl -sLo "$ISSH_PATH" "$ISSH_RAW_URL"
+# Push latest issh script to the device
+fetch_issh() {
+    local tmp
+    tmp="$(mktemp)"
 
-# Kill any running issh servers
-pkill toybox
+    dbg "Fetching the lastet issh script..."
+    curl -#Lo "$tmp" "$ISSH_RAW_URL"
 
-# Fork issh daemon for localhost connections only
-nice -n -20 -- sh "$ISSH_PATH" -dl &
+    dbg "Pushing issh to the device..."
+    adb push "$tmp" "$ISSH_PATH"
+
+    dbg "Cleaning up..."
+    rm -f "$tmp"
+}
+
+# Fork issh daemon
+bootstrap() {
+    dbg "Killing any existing issh processes..."
+    adb shell "pkill toybox"
+
+    dbg "Bootstrapping issh daemon..."
+    adb shell "nice -n -20 -- sh $ISSH_PATH -dl" &
+}
+
+prepare_env
+fetch_issh
+bootstrap
+dbg "Done."
